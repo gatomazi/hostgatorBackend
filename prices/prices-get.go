71c9d1a94f72a3df2a632b9ee3ae4780a549f990
Gabriel Tomazi <gabriel@stairs.studio>
2020-01-23 11:19:41 -0300

    Dockerfile created

--- prices/prices-get.go
+++ prices/prices-get.go
@@ -1,31 +1,42 @@
 package prices
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"testeH/models"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
 )
 
 //GetAll -
 func GetAll(c *gin.Context) {
-	// c.Request.URL.Query()
-	// var result *models.Product
-	var shared map[string]interface{} = make(map[string]interface{}, 0)
-	var products map[string]interface{} = make(map[string]interface{}, 0)
-	var planos map[string]interface{} = make(map[string]interface{}, 0)
 
-	PlanoP := models.Product{ID: 5, Name: "Plano P"}
-	PlanoM := models.Product{ID: 12, Name: "Plano M"}
-	PlanoTurbo := models.Product{ID: 16, Name: "Plano Turbo"}
+	db, err := sql.Open("mysql", "hostgator:hostgator123@tcp(172.18.0.2:3306)/teste")
+	// db, err = sql.Open("mysql", "XHFHkmpD8B:Mb6k8MT6RS@tcp(remotemysql.com)/XHFHkmpD8B")
 
-	planos["planoP"] = PlanoP
-	planos["planoM"] = PlanoM
-	planos["planoTurbo"] = PlanoTurbo
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	products["products"] = planos
-	shared["shared"] = products
-	c.JSON(http.StatusOK, shared)
+	// var shared map[string]interface{} = make(map[string]interface{}, 0)
+	// var products map[string]interface{} = make(map[string]interface{}, 0)
+	// var planos map[string]interface{} = make(map[string]interface{}, 0)
+
+	// PlanoP := models.Product{ID: 5, Name: "Plano P"}
+	// PlanoM := models.Product{ID: 12, Name: "Plano M"}
+	// PlanoTurbo := models.Product{ID: 16, Name: "Plano Turbo"}
+
+	// planos["planoP"] = PlanoP
+	// planos["planoM"] = PlanoM
+	// planos["planoTurbo"] = PlanoTurbo
+
+	// products["products"] = planos
+	// shared["shared"] = products
+	// c.JSON(http.StatusOK, shared)
 	return
 }
 
